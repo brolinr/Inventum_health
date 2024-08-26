75789832d56d35708ea7e54d1686fa6c584d4a1a
Brolin Remunyanga <remunyangabrolin@gmail.com>
2024-08-26 16:12:44 +0200

    Removed adress

--- app/ui/components/footer.tsx
+++ app/ui/components/footer.tsx
@@ -3,16 +3,20 @@ import Link from "next/link";
 import { mdiPhone, mdiLinkedin, mdiFacebook, mdiEmail } from "@mdi/js";
 
 const social_links = [
-  { username: '+263776409817', icon: mdiPhone, link: 'tel:+263776409817' },
-  { username: 'inventumzim@gmail.com', icon: mdiEmail, link: 'mailto:inventumzim@gmail.com' },
-  { username: 'Inventum ZW', icon: mdiLinkedin, link: '' },
-  { username: 'Inventum ZW', icon: mdiFacebook, link: '' }
+  { username: "+263776409817", icon: mdiPhone, link: "tel:+263776409817" },
+  {
+    username: "inventumzim@gmail.com",
+    icon: mdiEmail,
+    link: "mailto:inventumzim@gmail.com",
+  },
+  { username: "Inventum ZW", icon: mdiLinkedin, link: "" },
+  { username: "Inventum ZW", icon: mdiFacebook, link: "" },
 ];
 
 export default function Footer() {
   return (
     <footer className="w-full p-8 bg-white text-black">
-      <div className="md:w-4/5 mx-auto grid md:grid-cols-3 grid-cols-1 gap-4">
+      <div className="md:w-4/5 mx-auto grid md:grid-cols-2 grid-cols-1 gap-4">
         <div className="col-span-1 bg-slate-300 p-5 rounded-lg flex flex-col justify-center items-center">
           <h1 className="text-blue-600 font-black"> INVENTUM SERVICES </h1>
           <span className="text-black font-medium">HEALTH DIVISION</span>
@@ -20,45 +24,42 @@ export default function Footer() {
         </div>
 
         <div className="col-span-1 bg-slate-300 p-5 rounded-lg flex flex-col">
-          <h1 className="text-blue-600 mb-2 font-black text-center">Office Hours</h1>
-          <span className="text-sm text-black font-medium text-center">Mon - Thu: 8AM - 3PM</span>
-          <span className="text-sm font-medium text-center">Fri: 8AM - 1PM</span>
+          <h1 className="text-blue-600 mb-2 font-black text-center">
+            Office Hours
+          </h1>
+          <span className="text-sm text-black font-medium text-center">
+            Mon - Thu: 8AM - 3PM
+          </span>
+          <span className="text-sm font-medium text-center">
+            Fri: 8AM - 1PM
+          </span>
 
           <span className="text-sm lg:w-3/5 mx-auto text-center">
             Weekends: Closed
           </span>
         </div>
-
-        <div className="col-span-1 bg-slate-300 text-center p-5 rounded-lg flex flex-col">
-          <h1 className="text-blue-600 font-black mb-2"> Address </h1>
-          <span className="text-black text-sm">462 Gaydon Rd Helensvale</span>
-          <span className="text-black text-sm"> Harare, Zimbabwe</span>
-        </div>
       </div>
       <div className="lg:w-[90%] my-5 mx-auto md:rounded-full bg-gray-100 grid md:grid-cols-4 grid-cols-1">
-        {
-          social_links.map((link, index) => (
-            <div 
-              key={index}
-              className="
+        {social_links.map((link, index) => (
+          <div
+            key={index}
+            className="
                 flex-row gap-2 flex justify-self-center items-center hover:scale-95
                 h-fit duration-300 hover:bg-blue-200 py-2 px-4 rounded-full
               "
+          >
+            <Icon path={link.icon} size={1} color="currentColor" />
+            <Link
+              href={link.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black text-sm font-medium"
             >
-              <Icon path={link.icon} size={1} color="currentColor" />
-              <Link
-                href={link.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-black text-sm font-medium"
-              >
-                {link.username}
-              </Link>
-            </div>
-          ))
-        }
-        
+              {link.username}
+            </Link>
+          </div>
+        ))}
       </div>
     </footer>
-  )
+  );
 }
